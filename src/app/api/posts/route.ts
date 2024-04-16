6f4f5613301b96583cc4c0df67534363b4d4b7c3
AnisMorsli <anismorsli33@oulook.com>
2024-04-17 00:05:32 +0100

    add comp news

--- src/app/api/posts/route.ts
+++ src/app/api/posts/route.ts
@@ -1,7 +1,7 @@
 import { NextResponse } from "next/server";
 
 export async function GET() {
-  const res = await fetch("https://172.20.10.11:1337/api/posts");
+  const res = await fetch("http://172.20.10.11:1337/api/posts");
   const data = await res.json();
 
   return NextResponse.json({ data });
