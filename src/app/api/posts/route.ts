a029a927d12c0f2e097d8c1f281c964c146bc7a3
noahdev0 <96663091+noahdev0@users.noreply.github.com>
2024-04-17 00:02:30 +0100

    dede

--- src/app/api/posts/route.ts
+++ src/app/api/posts/route.ts
@@ -3,6 +3,7 @@ import { NextResponse } from "next/server";
 export async function GET() {
   const res = await fetch("http://172.20.10.11:1337/api/posts");
   const data = await res.json();
+  const { data: posts } = data;
 
-  return NextResponse.json(await res.json());
+  return NextResponse.json(posts);
 }
