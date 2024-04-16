ddb5ae92b2354f7bdb71840410becbb1ff3688d4
noahdev0 <96663091+noahdev0@users.noreply.github.com>
2024-04-16 15:46:01 +0100

    ded

--- src/components/blogs.tsx
+++ src/components/blogs.tsx
@@ -3,7 +3,16 @@ import React from "react";
 type Props = {};
 
 const Blogs = (props: Props) => {
-  return <div>Blogs</div>;
+  return (
+    <div>
+      <h1>Blogs</h1>
+      <Blog />
+    </div>
+  );
 };
 
 export default Blogs;
+
+const Blog = (props: Props) => {
+  return <div>Blogs</div>;
+};
