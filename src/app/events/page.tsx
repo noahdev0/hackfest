bdc04fd1e3c8126447b0e79ce4d3050666ed5f01
noahdev0 <96663091+noahdev0@users.noreply.github.com>
2024-04-17 08:14:50 +0100

    done

--- src/app/events/page.tsx
+++ src/app/events/page.tsx
@@ -1,8 +1,9 @@
 import Container from "@/components/container";
+import EventSection from "@/components/eventSection";
 import React from "react";
 
 const Events: React.FC = () => {
-  return <Container>Events page static </Container>;
+  return <Container><EventSection/></Container>;
 };
 
 export default Events;
