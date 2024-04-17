1c9ca3a04f28a59e0e0aec13f6807e4469f6646a
noahdev0 <96663091+noahdev0@users.noreply.github.com>
2024-04-17 04:38:02 +0100

    done with

--- src/components/blogs.tsx
+++ src/components/blogs.tsx
@@ -47,20 +47,20 @@ const Blogs = (props: Props) => {
               title={blog.attributes.title}
               description={blog.attributes.description}
               id={blog.id}
-
               photo={`${process.env.NEXT_PUBLIC_BASE_URL}${blog.attributes.photo?.data.attributes.url}`}
-
             />
           ))}
         </div>
-        <div className="flex justify-center">
-          <Link
-            className="inline-flex h-10 items-center justify-center rounded-md border border-gray-200  bg-white px-8 text-sm font-medium shadow-sm gap-2 transition-colors hover:bg-gray-100 hover:text-gray-900 focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-gray-950 disabled:pointer-events-none disabled:opacity-50 dark:border-gray-800  dark:bg-gray-950 dark:hover:bg-gray-950 dark:hover:text-gray-50 dark:focus-visible:ring-gray-300"
-            href="/blogs"
-          >
-            View all posts
-          </Link>
-        </div>
+        {props.blogsNumber && (
+          <div className="flex justify-center">
+            <Link
+              className="inline-flex h-10 items-center justify-center rounded-md border border-gray-200  bg-white px-8 text-sm font-medium shadow-sm gap-2 transition-colors hover:bg-gray-100 hover:text-gray-900 focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-gray-950 disabled:pointer-events-none disabled:opacity-50 dark:border-gray-800  dark:bg-gray-950 dark:hover:bg-gray-950 dark:hover:text-gray-50 dark:focus-visible:ring-gray-300"
+              href="/blogs"
+            >
+              View all posts
+            </Link>
+          </div>
+        )}
       </div>
     </section>
   );
