0ac0e8b35bd19af7067346e9203ceaeca372fa22
AnisMorsli <anismorsli33@oulook.com>
2024-04-17 02:35:58 +0100

    add comp news

---
+++ src/app/events/page.tsx
@@ -0,0 +1,8 @@
+import Container from "@/components/container";
+import React from "react";
+
+const Events: React.FC = () => {
+  return <Container>Events page static </Container>;
+};
+
+export default Events;
